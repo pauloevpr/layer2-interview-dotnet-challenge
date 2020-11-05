3ac234897f9e7aa4128d8558a05345934d028c17
Paulo Souza <psouza@layer2.com.au>
2020-11-05 17:56:37 -0300

    Initial drop

---
+++ Layer2.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Layer2.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ Layer2.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Layer2.Domain
+{
+    public class Class1
+    {
+    }
+}
